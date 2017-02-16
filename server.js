6d7d57851d7d670faaf465f06b8a4bcb794305b5
Achim Strauß <mail@achim-strauss.net>
2017-02-16 20:42:08 +0100

    functionality X

--- server.js
+++ server.js
@@ -25,6 +25,47 @@ slapp.message('robot', ['mention', 'direct_message'], (msg) => {
 })
 
 
+//*********************************************
+// Helpers
+//*********************************************
+const BI_DETECTOR = /([A-Za-z]+\-[1-9][0-9]*)/gi; //case insensitive (i) and multiple times (g)
+const JIRA_DEEP_LINK = "https://myclient.jira.com/browse/";
+
+var parseBacklogIdentifier = function(message) {
+    var matches = [];
+    var match = null;
+    while ((match = BI_DETECTOR.exec(message)) != null) {
+        matches.push(match[0]);
+    }
+    return matches;
+
+};
+
+var formatLink = function(backlogIdentifier) {
+    return "<" + JIRA_DEEP_LINK + backlogIdentifier + "|" + backlogIdentifier + ">";
+};
+
+//*********************************************
+// Setup different handlers for messages
+//*********************************************
+
+// Listen to any letters-dash-numbers
+slapp.message(/^.*([A-Za-z]+\-[1-9][0-9]*).*/i, ['ambient'], (msg, text) => {
+    var links = "";
+    var word = "Issue";
+    var backlogItems = parseBacklogIdentifier(text);
+    backlogItems.forEach(story => {
+        if (links !== "") {
+            word = "Issues";
+            links = links.concat(", ");
+        }
+        links = links.concat(formatLink(story));
+    });
+    msg.say("Let me help you with that :)")
+    msg.say("> " + word + ": " + links);
+});
+
+
 // attach Slapp to express server
 var server = slapp.attachToExpress(express())
 
